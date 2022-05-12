2dd7ccb3c023c725c6124eacb7a979e674ac8137
Tijani <96310128+aleepo@users.noreply.github.com>
2022-05-12 22:38:47 +0000

    Rename new -> New and next_token -> NextToken, because of go's weird requirement that first letter of public methods in a package that would be used in another package must be capitalized.

--- repl/repl.go
+++ repl/repl.go
@@ -11,20 +11,20 @@ import (
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer){
+func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
-		if !scanned{
+		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
-		l := lexer.new(line)
+		l := lexer.New(line)
 
-		for tok := l.next_token(); tok.Type != token.EOF; tok = l.next_token(){
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
 	}
