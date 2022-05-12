e525eb53fafa4cb4ce48b9db1f91ec75b2d24f8f
tijani <tijani@f6afcba9-9ef1-4bdd-9b72-7484f5705bac>
2022-05-12 22:39:29 +0000

    READ-EVALUATE-PRINT-LOOP now works and spits out tokens of input. YAY!!!

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"monkey/repl"
+)
+
+// TODO(tijani): add quit command.
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Welcome to the Monkey programming language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
