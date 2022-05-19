90a8da5e08a78cfc6f9383609afce16553c22eff
Tijani <96310128+aleepo@users.noreply.github.com>
2022-05-19 00:25:09 +0000

    Todos

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+/* TODO(tijani):
+ 
+ - add quit command to the repl.
+ - remove semicolons from the language to mark the end of a statement.
+ - add line and position reporting in the lexer when a lexing or parsing error occurs.
+*/
+
+
 package main
 
 import (
@@ -8,7 +16,6 @@ import (
 	"monkey/repl"
 )
 
-// TODO(tijani): add quit command.
 func main() {
 	user, err := user.Current()
 	if err != nil {
