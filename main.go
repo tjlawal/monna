f6e75902bd1f755309c4a34afe77eea4b085606e
Tijani <96310128+aleepo@users.noreply.github.com>
2022-05-23 14:18:27 +0000

    Do not show username at interpreter startup. Now Monk not monkey Formatting and code clarity

--- main.go
+++ main.go
@@ -11,17 +11,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/user"
 
 	"monkey/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+	// user, err := user.Current()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Printf("Hello %s! Welcome to the Monkey programming language!\n", user.Username)
+	fmt.Printf("Welcome to the Monk programming language!\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
