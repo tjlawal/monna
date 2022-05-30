5e22e2fd4ad5ff447e91e0e872aa5cbe35a26d6d
Tijani <96310128+aleepo@users.noreply.github.com>
2022-05-30 05:51:27 +0000

    ExpressionStatement in the AST to represent the typse of expressions that is supported in monkey. Hand written test in the AST to test the String() function for accuracy

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -14,6 +18,14 @@ type Statement interface {
 type Expression interface {
 	Node
 	expression_node()
+}
+
+func (l_program *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range l_program.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 type Program struct {
@@ -36,18 +48,43 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+	Token      token.Token // the first token in the expression
+	Expression Expression
+}
+
+// Let Statements
 func (ls *LetStatement) statement_node() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// Identifier
 func (i *Identifier) expression_node() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
+// Program
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -56,8 +93,33 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// Return Statements
 func (rs *ReturnStatement) statement_node() {}
 
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// Expression Statement
+func (es *ExpressionStatement) statement_node() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /* TODO(tijani):
- 
- - add quit command to the repl.
- - remove semicolons from the language to mark the end of a statement.
- - add line and position reporting in the lexer when a lexing or parsing error occurs.
-*/
 
+- add quit command to the repl.
+- remove semicolons from the language to mark the end of a statement.
+- add line and position reporting in the lexer when a lexing or parsing error occurs.
+*/
 
 package main
 
